d8114519ca812363de7e657db281834c4418a0c5
John Naylor <jonaylor89@gmail.com>
2018-12-26 01:18:46 -0500

    Chapter 2 in compiler in go

--- src/MonkeyInterpreter/repl/repl.go
+++ src/MonkeyInterpreter/repl/repl.go
@@ -1,10 +1,12 @@
 package repl
 
 import (
-	"MonkeyInterpreter/evaluator"
+//	"MonkeyInterpreter/evaluator"
 	"MonkeyInterpreter/lexer"
-	"MonkeyInterpreter/object"
+//  "MonkeyInterpreter/object"
 	"MonkeyInterpreter/parser"
+    "MonkeyInterpreter/compiler"
+    "MonkeyInterpreter/vm"
 	"bufio"
 	"fmt"
 	"io"
@@ -14,8 +16,8 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
-	env := object.NewEnvironment()
-	macroEnv := object.NewEnvironment()
+	// env := object.NewEnvironment()
+	// macroEnv := object.NewEnvironment()
 
 	for {
 		fmt.Printf(PROMPT)
@@ -36,6 +38,25 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
+        comp := compiler.New()
+        err := comp.Compile(program)
+        if err != nil {
+            fmt.Fprintf(out, "Compilation failure:\n %s\n", err) 
+            continue
+        }
+
+        machine := vm.New(comp.Bytecode())
+        err = machine.Run()
+        if err != nil {
+            fmt.Fprintf(out, "Bytecode execution failure:\n %s\n", err) 
+            continue
+        }
+
+        stackTop := machine.StackTop()
+        io.WriteString(out, stackTop.Inspect())
+        io.WriteString(out, "\n")
+        
+/*
 		evaluator.DefineMacros(program, macroEnv)
 		expanded := evaluator.ExpandMacros(program, macroEnv)
 
@@ -45,7 +66,7 @@ func Start(in io.Reader, out io.Writer) {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
 		}
-
+*/
 	}
 
 	fmt.Println("Au revoir")
