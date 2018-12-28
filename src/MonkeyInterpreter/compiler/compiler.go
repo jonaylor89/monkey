3a9a1a5f8900dc882abd30f713691773063913fe
John Naylor <jonaylor89@gmail.com>
2018-12-28 14:11:58 -0500

    gofmt

--- src/MonkeyInterpreter/compiler/compiler.go
+++ src/MonkeyInterpreter/compiler/compiler.go
@@ -7,24 +7,15 @@ import (
 	"fmt"
 )
 
-type EmittedInstruction struct {
-    Opcode      code.Opcode
-    Position    int
-}
-
 type Compiler struct {
-	instructions        code.Instructions
-	constants           []object.Object
-    lastInstruction     EmittedInstruction
-    previousInstruction EmittedInstruction
+	instructions code.Instructions
+	constants    []object.Object
 }
 
 func New() *Compiler {
 	return &Compiler{
-		instructions:           code.Instructions{},
-		constants:              []object.Object{},
-        lastInstruction:        EmittedInstruction{},
-        previousInstruction:    EmittedInstruction{},
+		instructions: code.Instructions{},
+		constants:    []object.Object{},
 	}
 }
 
@@ -43,24 +34,24 @@ func (c *Compiler) Compile(node ast.Node) error {
 		if err != nil {
 			return err
 		}
-        c.emit(code.OpPop)
+		c.emit(code.OpPop)
 
 	case *ast.InfixExpression:
-	
-        if node.Operator == "<" {
-            err := c.Compile(node.Right) 
-            if err != nil {
-                return err
-            }
 
-            err = c.Compile(node.Left)
-            if err != nil {
-                return err 
-            }
+		if node.Operator == "<" {
+			err := c.Compile(node.Right)
+			if err != nil {
+				return err
+			}
 
-            c.emit(code.OpGreaterThan)
-            return nil
-        }
+			err = c.Compile(node.Left)
+			if err != nil {
+				return err
+			}
+
+			c.emit(code.OpGreaterThan)
+			return nil
+		}
 
 		err := c.Compile(node.Left)
 		if err != nil {
@@ -75,120 +66,51 @@ func (c *Compiler) Compile(node ast.Node) error {
 		switch node.Operator {
 		case "+":
 			c.emit(code.OpAdd)
-        case "-":
-            c.emit(code.OpSub)
-        case "*":
-            c.emit(code.OpMul)
-        case "/":
-            c.emit(code.OpDiv)
-        case ">":
-            c.emit(code.OpGreaterThan)
-        case "==":
-            c.emit(code.OpEqual)
-        case "!=":
-            c.emit(code.OpNotEqual)
+		case "-":
+			c.emit(code.OpSub)
+		case "*":
+			c.emit(code.OpMul)
+		case "/":
+			c.emit(code.OpDiv)
+		case ">":
+			c.emit(code.OpGreaterThan)
+		case "==":
+			c.emit(code.OpEqual)
+		case "!=":
+			c.emit(code.OpNotEqual)
 
 		default:
 			return fmt.Errorf("unknown operator %s", node.Operator)
 		}
 
-    case *ast.PrefixExpression:
-        err := c.Compile(node.Right)
-        if err != nil {
-            return err 
-        }
+	case *ast.PrefixExpression:
+		err := c.Compile(node.Right)
+		if err != nil {
+			return err
+		}
 
-        switch node.Operator {
-            case "!":
-                c.emit(code.OpBang)
-            case "-":
-                c.emit(code.OpMinus)
-            default:
-                return fmt.Errorf("unknown opeartor %s", node.Operator)
-        }
-
-    case *ast.IfExpression:
-        err := c.Compile(node.Condition)
-        if err != nil {
-            return err 
-        }
-
-        jumpNotTruthyPos := c.emit(code.OpJumpNotTruthy, 9999) // 9999 is a placeholdler offset
-
-        err = c.Compile(node.Consequence)
-        if err != nil {
-            return err 
-        }
-
-        if c.lastInstructionIsPop() {
-            c.removeLastPop() 
-        }
-
-        jumpPos := c.emit(code.OpJump, 9999) // 9999 is a placeholder offset
-
-        afterConsequencePos := len(c.instructions)
-        c.changeOperand(jumpNotTruthyPos, afterConsequencePos)
-
-        if node.Alternative == nil {
-            c.emit(code.OpNull)
-        } else {
-            err := c.Compile(node.Alternative)
-            if err != nil {
-                return err 
-            }
-
-            if c.lastInstructionIsPop() {
-                c.removeLastPop() 
-            }
-        }
-
-        afterAlternativePos := len(c.instructions)
-        c.changeOperand(jumpPos, afterAlternativePos)
-
-     
-    case *ast.BlockStatement:
-        for _, s := range node.Statements {
-            err := c.Compile(s) 
-            if err != nil {
-                return err 
-            }
-        }
+		switch node.Operator {
+		case "!":
+			c.emit(code.OpBang)
+		case "-":
+			c.emit(code.OpMinus)
+		default:
+			return fmt.Errorf("unknown opeartor %s", node.Operator)
+		}
 
 	case *ast.IntegerLiteral:
 		integer := &object.Integer{Value: node.Value}
 		c.emit(code.OpConstant, c.addConstant(integer))
 
-    case *ast.Boolean:
-        if node.Value {
-            c.emit(code.OpTrue) 
-        } else {
-            c.emit(code.OpFalse) 
-        }
+	case *ast.Boolean:
+		if node.Value {
+			c.emit(code.OpTrue)
+		} else {
+			c.emit(code.OpFalse)
+		}
 	}
 
 	return nil
-}
-
-func (c *Compiler) replaceInstruction(pos int, newInstruction []byte) {
-    for i := 0; i < len(newInstruction); i++ {
-        c.instructions[pos+i] = newInstruction[i]
-    }
-}
-
-func (c *Compiler) changeOperand(opPos int, operand int) {
-    op := code.Opcode(c.instructions[opPos])
-    newInstruction := code.Make(op, operand)
-
-    c.replaceInstruction(opPos, newInstruction)
-}
-
-func (c *Compiler) lastInstructionIsPop() bool {
-    return c.lastInstruction.Opcode == code.OpPop
-}
-
-func (c *Compiler) removeLastPop() {
-    c.instructions = c.instructions[:c.lastInstruction.Position]
-    c.lastInstruction = c.previousInstruction
 }
 
 func (c *Compiler) addConstant(obj object.Object) int {
@@ -198,19 +120,10 @@ func (c *Compiler) addConstant(obj object.Object) int {
 
 func (c *Compiler) emit(op code.Opcode, operands ...int) int {
 	ins := code.Make(op, operands...)
+
 	pos := c.addInstruction(ins)
 
-    c.setLastInstruction(op, pos)
-
 	return pos
-}
-
-func (c *Compiler) setLastInstruction(op code.Opcode, pos int) {
-    previous := c.lastInstruction
-    last := EmittedInstruction{Opcode: op, Position: pos}
-
-    c.previousInstruction = previous
-    c.lastInstruction = last
 }
 
 func (c *Compiler) addInstruction(ins []byte) int {
--- src/MonkeyInterpreter/vm/vm_test.go
+++ src/MonkeyInterpreter/vm/vm_test.go
@@ -20,73 +20,55 @@ func TestIntergerArithmetic(t *testing.T) {
 		{"1", 1},
 		{"2", 2},
 		{"1 + 2", 3},
-        {"1 - 2", -1},
-        {"1 * 2", 2},
-        {"4 / 2", 2},
-        {"50 / 2 * 2 + 10 - 5", 55},
-        {"5 * (2 + 10)", 60},
-        {"5 + 5 + 5 + 5 - 10", 10},
-        {"2 * 2 * 2 * 2 * 2", 32},
-        {"5 * 2 + 10", 20},
-        {"5 + 2 * 10", 25},
-        {"5 * (2 + 10)", 60},
-        {"-5", -5},
-        {"-10", -10},
-        {"-50 + 100 + -50", 0},
-        {"(5 + 10 * 2 + 15 / 3) * 2 + -10", 50},
+		{"1 - 2", -1},
+		{"1 * 2", 2},
+		{"4 / 2", 2},
+		{"50 / 2 * 2 + 10 - 5", 55},
+		{"5 * (2 + 10)", 60},
+		{"5 + 5 + 5 + 5 - 10", 10},
+		{"2 * 2 * 2 * 2 * 2", 32},
+		{"5 * 2 + 10", 20},
+		{"5 + 2 * 10", 25},
+		{"5 * (2 + 10)", 60},
+		{"-5", -5},
+		{"-10", -10},
+		{"-50 + 100 + -50", 0},
+		{"(5 + 10 * 2 + 15 / 3) * 2 + -10", 50},
 	}
 
 	runVmTests(t, tests)
 }
 
 func TestBooleanExpressions(t *testing.T) {
-    tests := []vmTestCase{
-        {"true", true},
-        {"false", false},
-        {"1 < 2", true},
-        {"1 > 2", false},
-        {"1 < 1", false},
-        {"1 > 1", false},
-        {"1 == 1", true},
-        {"1 != 1", false},
-        {"1 == 2", false},
-        {"1 != 2", true},
-        {"true == true", true},
-        {"false == false", true},
-        {"true == false", false},
-        {"true != false", true},
-        {"false != true", true},
-        {"(1 > 2) == true", false},
-        {"(1 < 2) == false", false},
-        {"(1 > 2) == true", false},
-        {"(1 > 2) == false", true},
-        {"!true", false},
-        {"!false", true},
-        {"!5", false},
-        {"!!true", true},
-        {"!!false", false},
-        {"!!5", true},
-        {"!(if (false) { 5; })", true},
-    }
+	tests := []vmTestCase{
+		{"true", true},
+		{"false", false},
+		{"1 < 2", true},
+		{"1 > 2", false},
+		{"1 < 1", false},
+		{"1 > 1", false},
+		{"1 == 1", true},
+		{"1 != 1", false},
+		{"1 == 2", false},
+		{"1 != 2", true},
+		{"true == true", true},
+		{"false == false", true},
+		{"true == false", false},
+		{"true != false", true},
+		{"false != true", true},
+		{"(1 > 2) == true", false},
+		{"(1 < 2) == false", false},
+		{"(1 > 2) == true", false},
+		{"(1 > 2) == false", true},
+		{"!true", false},
+		{"!false", true},
+		{"!5", false},
+		{"!!true", true},
+		{"!!false", false},
+		{"!!5", true},
+	}
 
-    runVmTests(t, tests)
-}
-
-func TestConditionals(t *testing.T) {
-    tests := []vmTestCase{
-        {"if (true) { 10 }", 10},
-        {"if (true) { 10 } else { 20 }", 10},
-        {"if (false) { 10 } else { 20 }", 20},
-        {"if (1) { 10 }", 10},
-        {"if (1 < 2) { 10 }", 10},
-        {"if (1 < 2) { 10 } else { 20 }", 10},
-        {"if (1 > 2) { 10 } else { 20 }", 20},
-        {"if (1 > 2) { 10 }", Null},
-        {"if (false) { 10 }", Null},
-        {"if ((if (false) { 10 })) { 10 } else { 20 }", 20},
-    }
-
-    runVmTests(t, tests)
+	runVmTests(t, tests)
 }
 
 func runVmTests(t *testing.T, tests []vmTestCase) {
@@ -128,15 +110,11 @@ func testExpectedObject(
 			t.Errorf("testIntegerObject failed: %s", err)
 		}
 
-    case bool:
-        err := testBooleanObject(bool(expected), actual)
-        if err != nil {
-            t.Errorf("testBooleanObject failed: %s", err) 
-        }
-    case *object.Null:
-        if actual != Null {
-            t.Errorf("object is not NULL: %T (%+v)", actual, actual)    
-        }
+	case bool:
+		err := testBooleanObject(bool(expected), actual)
+		if err != nil {
+			t.Errorf("testBooleanObject failed: %s", err)
+		}
 	}
 
 }
@@ -163,16 +141,16 @@ func testIntegerObject(expected int64, actual object.Object) error {
 }
 
 func testBooleanObject(expected bool, actual object.Object) error {
-    result, ok := actual.(*object.Boolean)
-    if !ok {
-        return fmt.Errorf("object is not Boolean. got=%T (%+v)",
-                            actual, actual) 
-    }
+	result, ok := actual.(*object.Boolean)
+	if !ok {
+		return fmt.Errorf("object is not Boolean. got=%T (%+v)",
+			actual, actual)
+	}
 
-    if result.Value != expected {
-        return fmt.Errorf("object has wrong value. got=%t, want=%t",
-                            result.Value, expected) 
-    }
+	if result.Value != expected {
+		return fmt.Errorf("object has wrong value. got=%t, want=%t",
+			result.Value, expected)
+	}
 
-    return nil
+	return nil
 }
